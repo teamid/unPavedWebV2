e68b910e826b91efeeda95a956c007dd9f47e4b1
David Smith <teamid@gmail.com>
2015-01-13 13:42:51 -0800

    started work on profile ile page

--- js/new-app.js
+++ js/new-app.js
@@ -9,11 +9,59 @@ $(document).ready(function() {
 		"top" : (nav_height) +"px"
 	});
 
+	$("#profile-container").css({
+		"top" : (nav_height) +"px"
+	});
+
 	$("#sidebar").css({
 		"top" : (nav_height) +"px"
 	});
 
 	$("#sidebar").height(window_height - nav_height)
+
+
+
+	//CENTER USER CONTAINER
+
+	$("#user-container").css({
+		"width" : (window_width*0.93906) +"px",
+		"left" : (window_width*0.5) +"px",
+		"top" : (window_width*0.01875 + nav_height) +"px"
+	});
+
+
+/*
+	$("#user-photos-list > li > img").css({
+		"width" : (window_width*0.93906*0.1859375) +"px",
+		"height" : (window_width*0.93906*0.1859375) +"px"
+	});
+*/
+
+	/*
+	$("#user-photos-list").css({
+		"height" : (window_width*0.93906*0.20) +"px"
+	});
+
+	$("#first-photo, #second-photo").css({
+		"width" : (window_width*0.93906*0.20) +"px"
+	});
+*/ 
+	var user_face_width = window_width*0.09765625
+
+
+
+	$("#user-face").css({
+		"width" : user_face_width +"px",
+		"height" : user_face_width +"px",
+		"top" : "-" + (window_width*0.10546875*0.5 + 4) +"px",
+	
+	});
+
+
+
+
+
+
 
 	//CENTER NAV LOGO
 	$("#nav-logo").css({
